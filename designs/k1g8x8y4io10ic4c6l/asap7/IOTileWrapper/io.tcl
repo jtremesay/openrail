869224a1d765568b40665cef8caf4807f1e81f2d
Jonathan Tremesaygues <jonathan.tremesaygues@slaanesh.org>
2023-09-21 23:47:42 +0200

    remove iotile

--- designs/k1g8x8y4io10ic4c6l/asap7/IOTileWrapper/io.tcl
+++
@@ -1,4 +0,0 @@
-set_io_pin_constraint -region top:* -pin_names [list data_north_in data_north_out config_out]
-set_io_pin_constraint -region bottom:* -pin_names [list data_south_out data_south_in config_in]
-set_io_pin_constraint -region left:* -pin_names [list data_west_in data_west_out clock nreset]
-set_io_pin_constraint -region right:* -pin_names [list data_east_out data_east_in config_enable config_nreset]
