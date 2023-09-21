c8f6a5b4e719122d243cf33b9eaacd715bd70a04
Jonathan Tremesaygues <jonathan.tremesaygues@slaanesh.org>
2023-09-21 23:48:43 +0200

    reorder pins

--- designs/k1g8x8y4io10ic4c6l/asap7/IOSouthTileWrapper/io.tcl
+++ designs/k1g8x8y4io10ic4c6l/asap7/IOSouthTileWrapper/io.tcl
@@ -1,4 +1,4 @@
-set_io_pin_constraint -region top:* -pin_names [list data_from_ic data_to_ic config_out]
-set_io_pin_constraint -region bottom:* -pin_names [list data_from_io data_to_io config_in]
-set_io_pin_constraint -region left:* -pin_names [list clock nreset]
+set_io_pin_constraint -region top:* -pin_names [list data_to_ic data_from_ic config_out]
+set_io_pin_constraint -region bottom:* -pin_names [list config_in data_from_io data_to_io]
+set_io_pin_constraint -region left:* -pin_names [list clock]
 set_io_pin_constraint -region right:* -pin_names [list config_enable config_nreset]
--- designs/k1g8x8y4io10ic4c6l/asap7/LogicTileWrapper/io.tcl
+++ designs/k1g8x8y4io10ic4c6l/asap7/LogicTileWrapper/io.tcl
@@ -1,4 +1,4 @@
-set_io_pin_constraint -region top:* -pin_names [list data_north_in data_north_out config_out]
-set_io_pin_constraint -region bottom:* -pin_names [list data_south_out data_south_in config_in]
+set_io_pin_constraint -region top:* -pin_names [list data_north_out data_north_in config_out]
+set_io_pin_constraint -region bottom:* -pin_names [list config_in data_south_in data_south_out]
 set_io_pin_constraint -region left:* -pin_names [list data_west_in data_west_out clock nreset]
 set_io_pin_constraint -region right:* -pin_names [list data_east_out data_east_in config_enable config_nreset]
